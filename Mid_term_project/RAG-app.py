d37d69d2ab6f934527f4e5f2d0410de37fd5d7e6
Alejandra Manrique <amanriqueo98@gmail.com>
2025-05-12 19:59:23 -0500

    debugging for app

--- Mid_term_project/RAG-app.py
+++ Mid_term_project/RAG-app.py
@@ -16,8 +16,7 @@ st.title("💬 The University of Chicago Master's in Applied Data Science Chatbo
 
 # --- Load fine-tuned embedding model ---
 embedding_model = HuggingFaceEmbeddings(
-    model_name="fine_tuned_qa_embedding_model",
-    device="cpu"
+    model_name="fine_tuned_qa_embedding_model"
 )
 
 # --- Load persisted vectorstore ---
